77e5e8eba182d89d33243283bcfe78505d74efca
leotranq <leotranq@gmail.com>
2022-02-08 13:47:40 +0000

    update

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import "../../styles/NavBar.css";
+import "/workspace/StarWarBlog/src/img/StarWarsFont.ttf"
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+
+		<div className="NavBarBody">
+
+			<div className="NavBar">
+				<h1 className="Tittle"><strong>Star Wars</strong></h1>
+				<button className="navButton" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navBarIcon"><i className="fab fa-jedi-order" id="Icon"></i></span>
+				</button>
+				
 			</div>
-		</nav>
-	);
+		</div>
+			);
 };
